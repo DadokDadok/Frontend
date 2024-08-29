6a9079b17f095396d69ee551f6d64a422bcd6de5
onebbu <vmfdo23@gmail.com>
2024-08-29 16:39:30 +0900

    UPDATE: 세팅 테스트용 데이터 추가

--- pl_front/src/pages/testPage/TestPage.jsx
+++ pl_front/src/pages/testPage/TestPage.jsx
@@ -3,13 +3,14 @@ import {useEffect, useState} from "react";
 import axios from "axios";
 
 export default function TestPage() {
-
-    const [data, setData] = useState([null]);
+    const [data, setData] = useState(null);
+    const [inputValue, setInputValue] = useState('');
+    const [responseMessage, setResponseMessage] = useState('');
 
     useEffect(() => {
         async function fetchData() {
             try {
-                const result = await axios.get(`/test/1`);
+                const result = await axios.get(`/api/test/1`);
                 setData(result.data);
                 console.log(result.data);
             } catch (error) {
@@ -18,12 +19,42 @@ export default function TestPage() {
         }
 
         fetchData();
-
     }, []);
+
+    const handleChange = (event) => {
+        setInputValue(event.target.value);
+    };
+
+    const handleSubmit = async () => {
+        try {
+            const res = await axios.post('/api/test/submit', { input: inputValue });
+            console.log("sussess", res);
+            setResponseMessage(res.data);
+        } catch (error) {
+            console.error("Error submitting data:", error);
+            setResponseMessage("Error submitting data.");
+        }
+    };
 
     return(
         <>
-            {data}
+            <div>
+                <h1>서버 Get 응답:</h1>
+                <p>{data}</p>
+            </div>
+            <div>
+                <input
+                    type="text"
+                    value={inputValue}
+                    onChange={handleChange}
+                    placeholder="Enter something"
+                />
+                <button onClick={handleSubmit}>Submit</button>
+                <div>
+                    <h2>서버 Post 응답:</h2>
+                    <p>{responseMessage}</p>
+                </div>
+            </div>
         </>
 
     );
